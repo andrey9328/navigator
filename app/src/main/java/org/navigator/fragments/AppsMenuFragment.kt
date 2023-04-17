1e8a4f250e503684e7866cf146008ebed0082c04
Andrey <andrey93.28@gmail.com>
2023-04-17 20:29:47 +0300

    Fix demo app

--- app/src/main/java/org/navigator/fragments/AppsMenuFragment.kt
+++ app/src/main/java/org/navigator/fragments/AppsMenuFragment.kt
@@ -5,11 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import org.navigator.app.R
-import org.navigator.main.models.NavigationScreen
+import org.navigator.main.actions.NavBackToRootScreen
 import org.navigator.main.actions.NavCreateSubNavigator
+import org.navigator.main.actions.NavOpenScreen
 import org.navigator.main.getRouter
+import org.navigator.main.models.NavigationScreen
 
 class AppsMenuFragment: Fragment() {
 
@@ -23,13 +26,31 @@ class AppsMenuFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<Button>(R.id.btnOpen)?.setOnClickListener {
-            getRouter(null).addAction(NavCreateSubNavigator(
-                R.id.historyMenu.toString(),
-                "1",
-                NavigationScreen { LoginFragment() },
-                isStayCurrentTab = false)
-            )
+        view.findViewById<TextView>(R.id.tvExample)?.let {
+            if (savedInstanceState?.getString("Text") == null) {
+                it.text = "AppsMenuFragment " + parentFragmentManager.backStackEntryCount.toString()
+            } else {
+                it.text = savedInstanceState.getString("Text")
+            }
         }
+        view.findViewById<Button>(R.id.btnBackParent).setOnClickListener {
+            getRouter().addAction(NavBackToRootScreen)
+        }
+
+        view.findViewById<Button>(R.id.btnOpen).setOnClickListener {
+            getRouter().addAction(NavOpenScreen(NavigationScreen { AppsMenuFragment() }))
+
+            view.findViewById<Button>(R.id.closeActivity).visibility =
+                if(arguments?.containsKey("close") == true) View.VISIBLE else View.GONE
+
+            view.findViewById<Button>(R.id.closeActivity)?.setOnClickListener {
+                requireActivity().finish()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("Text", view?.findViewById<TextView>(R.id.tvExample)?.text.toString())
     }
 }
--- app/src/main/java/org/navigator/fragments/HistoryFragment.kt
+++ app/src/main/java/org/navigator/fragments/HistoryFragment.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import org.navigator.app.R
+import org.navigator.main.actions.NavOpenScreen
+import org.navigator.main.actions.NavSelectTab
+import org.navigator.main.getRouter
+import org.navigator.main.models.NavigationScreen
 
 
 class HistoryFragment: Fragment() {
@@ -20,6 +28,35 @@ class HistoryFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.tvExample)?.let {
+            if (savedInstanceState?.getString("Text") == null) {
+                it.text = "History fragment " + parentFragmentManager.backStackEntryCount.toString()
+            } else {
+                it.text = savedInstanceState.getString("Text")
+            }
 
+        }
+        view.findViewById<Button>(R.id.btnOpenProfile).setOnClickListener {
+            val inputText = view.findViewById<EditText>(R.id.etArguments).text
+            if (inputText.isNullOrEmpty()) {
+                getRouter(null).addAction(NavSelectTab(R.id.profileMenu.toString()))
+            } else {
+                getRouter(null).addAction(
+                    NavSelectTab(
+                        R.id.profileMenu.toString(), args =
+                        bundleOf("Arg" to inputText.toString())
+                    )
+                )
+            }
+        }
+
+        view.findViewById<Button>(R.id.btnOpen).setOnClickListener {
+            getRouter().addAction(NavOpenScreen(NavigationScreen { HistoryFragment() }))
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("Text", view?.findViewById<TextView>(R.id.tvExample)?.text.toString())
     }
 }
