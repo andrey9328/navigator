86805aa00bc81c38fc53f8c221d072a777a49b48
andre <amdrey93.28@gmail.com>
2023-12-10 22:43:26 +0200

    Add description

--- navigator/src/main/java/org/navigator/main/models/NavigationScreen.kt
+++ navigator/src/main/java/org/navigator/main/models/NavigationScreen.kt
@@ -1,5 +1,8 @@
 package org.navigator.main.models
 
 import androidx.fragment.app.Fragment
+/**
+ * @param screenKey use for added and search screens in fragment backstack if screen key is null use fragment::class.name
+ */
 
 data class NavigationScreen(val screenKey: String? = null, val fragment: () -> Fragment)
